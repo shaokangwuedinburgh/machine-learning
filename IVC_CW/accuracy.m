b5bb89de5a1db9fc4f75430ce5dda29d7b93b6d8
shaokangwuedinburgh <77343890+shaokangwuedinburgh@users.noreply.github.com>
2021-03-08 21:29:32 +0800

    Delete IVC_CW directory
    Delete

--- IVC_CW/accuracy.m
+++
@@ -1,8 +0,0 @@
-function accuracy = accuracy(Prediction, imdsTest)
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-Prediction = string(Prediction);
-TrueLabel = string(imdsTest.Labels);
-accuracy = sum(strcmp(Prediction, TrueLabel))/length(imdsTest.Labels);
-end
-
